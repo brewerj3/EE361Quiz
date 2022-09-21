04ab579e158cc0fcff7b8db5d0f72e097664e8fc
brewerj3 <joshuabrewer784@gmail.com>
2022-09-21 07:35:02 -1000

    Initial commit

---
+++ barely.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int R[32];  /* These are the only variables */
+
+void main() {
+    R[0] = 0;   /* This is always zero; it's read only */
+
+/* Start of code to convert to BarelyC */
+    R[1] = 0;
+    for (R[2] = 1; R[2] < 10; R[2]++) {
+        if (R[2] > 3) {
+            R[1] = R[1] + R[2];
+        }
+    }
+/* End of the code to convert to BarelyC */
+
+    printf("R[1] = %d\n",R[1]);
+}
